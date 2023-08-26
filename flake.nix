0bccfe77a44c4dfd07536ad578fe2d3e62f180d1
sarahec <sarahec@nextquestion.net>
2023-08-26 16:00:57 -0700

    Add missing environment variables for Prisma

--- flake.nix
+++ flake.nix
@@ -17,7 +17,12 @@
           pkgs.openssl
           pkgs.sqlite
         ];
-        shellHook = ''
+        shellHook = with pkgs; ''
+          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
           export CYPRESS_INSTALL_BINARY=0
           export CYPRESS_RUN_BINARY=${pkgs.cypress}/bin/Cypress
           npm set prefix ~/.npm-global
