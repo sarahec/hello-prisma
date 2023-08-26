b7749d23a15944cc513f8b4bd596c7773966769e
sarahec <sarahec@nextquestion.net>
2023-08-26 15:40:33 -0700

    Use Prisma 5.0.0 to match nixos unstable

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('Hello World');
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+    },
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
